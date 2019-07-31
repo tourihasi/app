d591487d4d8af652cf8c0dc7c5127847b7ec3562
avexbesuke <hinshitsu@avex-inc.co.jp>
2019-07-31 09:21:18 +0900

    'UserModelAddValidation'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
+    if @user.save
       redirect_to users_path
     else
+      render :new
     end
   end
 
