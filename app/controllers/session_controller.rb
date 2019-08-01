e8b555ebb21338cc3b3bf3409778887275d8b658
avexbesuke <hinshitsu@avex-inc.co.jp>
2019-08-01 18:40:01 +0900

    '間違ってappfile消してしまっていたためappfile復旧'

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,40 @@
+class SessionController < ApplicationController
+  skip_before_action :login_required
+
+  def new
+  end
+
+  def create
+    user = nil
+    user = User.find_by(name: session_params[:name])
+
+    if user&.authenticate(session_params[:password]) #authenticate = 暗号化されてないパスワードとpassword_digest属性値の一致を検証
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  # google認証
+  def create_g
+    user = User.from_omniauth(request.env["omniauth.auth"])
+    # binding.pry
+    if user.save
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:name, :password)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ApplicationRecord
+  require 'date'
+  require 'securerandom'
+
+  #password_digest 有効
+  has_secure_password
+
+  #バリデーション
+  validates :name,  presence: true #nameが空でないこと
+  validates :email,  presence: true
+  validates :password,  presence: true
+  validates :name, uniqueness: true #nameがﾕﾆｰｸであること
+  validates :password, length: { in: 6..40 } #password6文字以上40字以内
+
+  # google 認証
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      # 元々userモデルにあったｶﾗﾑ
+      user.name = auth.info.name # null false
+      user.email = auth.info.email # null false
+      user.admin = true # null false
+      user.created_at = Date.today
+      user.updated_at = Date.today
+      user.password = SecureRandom.hex(16) #null false だから何か入れてやらないとｴﾗｰに、、、
+
+      #追加したｶﾗﾑ
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.image = auth.info.image
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      return user
+    end
+
+  end
+
+
+end
