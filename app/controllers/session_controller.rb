74f5aa43ad931c9a552d9694e9f6d7ec8734954e
avexbesuke <xyezsbf3@i.softbank.jp>
2019-08-08 21:45:24 +0900

    sidekiqを使用した非同期処理

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,6 +8,8 @@ class SessionController < ApplicationController
     user = User.find_by(name: session_params[:name])
     # binding.pry
     if user&.authenticate(session_params[:password]) # authenticate = 暗号化されてないパスワードとpassword_digest属性値の一致を検証
+      SampleJob.perform_later #ActiveJob:  サンプルジョブをperform_later = できる時にやってね
+      SampleJob.set(wait_until: Date.tomorrow.noon).perform_later #翌日の正午=tomorrow.noon に実行
       session[:user_id] = user.id
       redirect_to root_path
     else
